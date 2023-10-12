76205753490d7d1a11f72a978d2ab6ff79b942fe
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-12 15:30:49 +0300

    Initial commit

---
+++ src/main/java/by/flameksandr/practical/coding/practicalcoding/PracticalCodingApplication.java
@@ -0,0 +1,13 @@
+package by.flameksandr.practical.coding.practicalcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/flameksandr/practical/coding/practicalcoding/PracticalCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flameksandr.practical.coding.practicalcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
