d77a2578aba28a9398b8b0601bbf3336b2a72333
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-12 16:30:10 +0300

    Can we write a JPA query to sort employees based on employee names using spring data jpa? 2. Update the EmployeeService class to call the custom query from the repository.

--- src/main/java/by/flameksandr/practical/coding/practicalcoding/service/EmployeeService.java
+++ src/main/java/by/flameksandr/practical/coding/practicalcoding/service/EmployeeService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -30,5 +31,9 @@ public class EmployeeService {
 
     public void deleteEmployeeById(Long id) {
         employeeRepository.deleteById(id);
+    }
+
+    public List<Employee> getAllEmployeesAndSortByNameStartingWithA() {
+        return employeeRepository.findAllAndSortByNameStartingWithA();
     }
 }
